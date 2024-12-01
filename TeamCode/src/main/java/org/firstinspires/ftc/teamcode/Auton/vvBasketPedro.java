7d08be5ad4e649f53cb764b037a535a89cea90c6
CoachPrice <vanhoosenvikingsftc@gmail.com>
2024-12-01 18:38:24 -0500

    27Nov Pedro updates plus printed claw teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/vvBasketPedro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/vvBasketPedro.java
@@ -46,14 +46,15 @@ public class vvBasketPedro extends OpMode {
 
     private int pathState;
 
-    // We want to start the bot at x: 14, y: -60, heading: 90 degrees (these are FIRST coordinates)
+    // We want to start the bot at x: 65, y: 7, heading: 90 degrees (these are FIRST coordinates)
     private Pose startPose = new Pose(65, 7, Math.toRadians(90));
     // all sample mark locations
-    private Pose highchamber = new Pose(65,33.5);
-    private Pose sampleMark1 = new Pose(22.5,43);
-    private Pose sampleMark2 = new Pose(12.5,43);
-    private Pose sampleMark3 = new Pose(2.5,43,Math.toRadians(180));
-    private Pose dropposition = new Pose (16,16,Math.toRadians(45));
+    private Pose highchamber = new Pose(65,36);
+    private Pose sampleMark1 = new Pose(31,40);
+    private Pose sampleMark2 = new Pose(16,38);
+    private Pose sampleMark3 = new Pose(9.5,53,Math.toRadians(180));
+    private Pose dropposition = new Pose (22,16,Math.toRadians(45));
+    private Pose dropposition2 = new Pose (16,16, Math.toRadians(45));
     private Pose specimenMark1 = new Pose(121.5, 43);
     private Pose specimenMark2 = new Pose(131.5, 43);
     private Pose specimenMark3 = new Pose(141.5, 43,Math.toRadians(0));
@@ -67,7 +68,7 @@ public class vvBasketPedro extends OpMode {
         return follower.getCurrentTValue() > 0.5;
     }
     public boolean atPathEnd() {
-        return follower.getCurrentTValue() > 0.99;
+        return follower.getCurrentTValue() > 0.95;
     }
     public boolean isAtEndOfPathAndNotMoving() { //Robot seems unstable with control, along with isBusy
         return atPathEnd() && follower.getVelocityMagnitude() < 0.1;
@@ -88,7 +89,7 @@ public class vvBasketPedro extends OpMode {
 
         //Path from submersible to the first yellow sample, approaches straight on after a sweep
         yellow1 = follower.pathBuilder()
-                .addPath(new BezierCurve(new Point(highchamber.getX(), highchamber.getY(),Point.CARTESIAN), new Point(47,16, Point.CARTESIAN), new Point(sampleMark1.getX(),sampleMark1.getY()-botPickup, Point.CARTESIAN)))
+                .addPath(new BezierCurve(new Point(highchamber.getX(), highchamber.getY(),Point.CARTESIAN), new Point(60,16, Point.CARTESIAN), new Point(sampleMark1.getX(),sampleMark1.getY()-botPickup, Point.CARTESIAN)))
                 .setConstantHeadingInterpolation(startPose.getHeading())
                 .setPathEndTimeoutConstraint(3)
                 .build();
@@ -106,180 +107,224 @@ public class vvBasketPedro extends OpMode {
                 .build();
 
         yellow2drop = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(sampleMark2.getX(),sampleMark2.getY()-botPickup, Point.CARTESIAN), new Point(dropposition.getX(),dropposition.getY(), Point.CARTESIAN)))
-                .setLinearHeadingInterpolation(startPose.getHeading(),dropposition.getHeading(),0.5)
-                .setPathEndTimeoutConstraint(0)
+                .addPath(new BezierLine(new Point(sampleMark2.getX(),sampleMark2.getY()-botPickup, Point.CARTESIAN), new Point(dropposition2.getX(),dropposition2.getY(), Point.CARTESIAN)))
+                .setLinearHeadingInterpolation(startPose.getHeading(),dropposition2.getHeading(),0.5)
+                .setPathEndTimeoutConstraint(2)
                 .build();
 
         yellow3 = follower.pathBuilder()
-                .addPath(new BezierCurve(new Point(dropposition.getX(), dropposition.getY(),Point.CARTESIAN), new Point(dropposition.getX()+32, dropposition.getY()+24,Point.CARTESIAN), new Point(sampleMark3.getX()+16,sampleMark3.getY(), Point.CARTESIAN)))
+                .addPath(new BezierCurve(new Point(dropposition2.getX(), dropposition2.getY(),Point.CARTESIAN), new Point(dropposition2.getX()+32, dropposition2.getY()+24,Point.CARTESIAN), new Point(sampleMark3.getX()+16,sampleMark3.getY(), Point.CARTESIAN)))
                 .setLinearHeadingInterpolation(dropposition.getHeading(),sampleMark3.getHeading(),0.5)
                 .addPath(new BezierLine(new Point(sampleMark3.getX()+16,sampleMark3.getY(), Point.CARTESIAN), new Point(sampleMark3.getX()+botPickup,sampleMark3.getY(), Point.CARTESIAN)))
                 .setConstantHeadingInterpolation(sampleMark3.getHeading())
-                .setPathEndTimeoutConstraint(0)
+                .setPathEndTimeoutConstraint(2)
                 .build();
 
         yellow3drop = follower.pathBuilder()
                 .addPath(new BezierLine(new Point(sampleMark3.getX()+botPickup,sampleMark3.getY(), Point.CARTESIAN), new Point(dropposition.getX(),dropposition.getY(), Point.CARTESIAN)))
                 .setLinearHeadingInterpolation(sampleMark3.getHeading(),dropposition.getHeading(),0.5)
-                .setPathEndTimeoutConstraint(0)
+                .setPathEndTimeoutConstraint(2)
                 .build();
 
         ascent = follower.pathBuilder()
                 .addPath(new BezierCurve(new Point(dropposition.getX(), dropposition.getY(),Point.CARTESIAN), new Point(ascentPose.getX(),ascentPose.getY(), Point.CARTESIAN)))
                 .setLinearHeadingInterpolation(dropposition.getHeading(),ascentPose.getHeading(),0.5)
-                .setPathEndTimeoutConstraint(0)
+                .setPathEndTimeoutConstraint(2)
                 .build();
     }
 
     public void autonPathUpdate() {
         switch (pathState) {
-            case 9: //move arm to position
-                robot.rgb.setPosition(0.5);
-                robot.armPos(robot.armHighCa, robot.armEPower);
-                //robot.extArmPos(robot.extArmHighCe, robot.extArmEPower);
-                robot.moveWristHighCw();
 
-                if (pathTimer.getElapsedTime()>500) {
+        /*    case 0:
+                follower.followPath(fwdHighCmbr);
+                setPathState(1);
+                break;
+            case 1:
+                if (pathTimer.getElapsedTime() > 1) {
+                    robot.rgb.setPosition(0.5);
+                    robot.armPos(robot.armHighCaNew, robot.armEPower);
+                    robot.moveWristHighCwNew();
+                    robot.extArmPos(robot.extArmHighCe, robot.extArmEPower);
+                    setPathState(2);
+                }
+                break;
+            case 2:
+                if (follower.getPose().getX() > (highchamber.getX() - 1) && follower.getPose().getY() > (highchamber.getY() - 1)) {
+                    robot.openClaw();
+                }
+                break;
+*/
+            case 6: //move arm to position and high chamber specimen placment
+                if (follower.getPose().getY() > (startPose.getY() - 1)) {
+                    robot.rgb.setPosition(0.5);
+                    robot.armPos(robot.armHighCaNew, robot.armEPower);
+                    robot.moveWristHighCwNew();
+                    robot.extArmPos(robot.extArmHighCe, robot.extArmEPower);
+                    setPathState(7);
+                }
+                break;
+
+            case 7: //high chamber path
+                if (pathTimer.getElapsedTime() > 1500) {
+                    follower.followPath(fwdHighCmbr);
+
+                    setPathState(8);
+                }
+                break;
+
+            case 8: // High chamber open claw
+                if (follower.getPose().getX() > (highchamber.getX() - 1) && follower.getPose().getY() > (highchamber.getY() - 1)) {
+                    robot.openClaw();
+                    //robot.moveWristFloor();
+
+                    setPathState(9);
+                }
+
+                break;
+            case 9: // High chamber retract arm
+                if (pathTimer.getElapsedTime() > 500) {
+                    robot.extArmPos(0, robot.extArmEPower);
+
                     setPathState(10);
                 }
                 break;
 
-            case 10: //high chamber specimen placement
-                follower.followPath(fwdHighCmbr);
+            case 10: // yellow1 path
+                if (pathTimer.getElapsedTime() > 1000) {
+                    follower.followPath(yellow1);
 
-                if (pathTimer.getElapsedTime()>1500) {
                     setPathState(11);
                 }
-
                 break;
 
-            case 11: // Yellow1
-                if (pathTimer.getElapsedTime() > 1000) {
-                    robot.armPos(robot.armHighCa - 300, 0.4);
-                    if (pathTimer.getElapsedTime() > 1500) {
-                        robot.openClaw();
-                    }
-
-                    //robot.extArmPos(50, robot.extArmEPower);
-                    if (pathTimer.getElapsedTime() > 1600) {
-                        follower.followPath(yellow1);
-                    }
-
-                    if (atPathEnd() && pathTimer.getElapsedTime()>2000) {
-                        setPathState(12);
-                    }
-                }
-
-                break;
-
-            case 12: //Yellow1pick
-                if (pathTimer.getElapsedTime() > 100) {
+            case 11: //Yellow1 pickup
+                //if (follower.getPose().getX() > (sampleMark1.getX() - 1) && follower.getPose().getY() > (sampleMark1.getY() - 1))
+                if (pathTimer.getElapsedTime() > 500) {
                     robot.pickSample();
+                    setPathState(12);
+                }
+                break;
 
-                    if (armSetDown() && pathTimer.getElapsedTime() > 1000) { // && pathTimer.getElapsedTime() > 2000
-                        robot.closeClaw();
-
-                        setPathState(13);
-                    }
+            case 12: //Yellow1 close claw
+                if (pathTimer.getElapsedTime() > 1250) {
+                    robot.closeClaw();
+                    //follower.followPath(yellow1drop);
+                    setPathState(13);
                 }
 
-                    break;
+                break;
 
-            case 13: //Yellow1drop
-                if (pathTimer.getElapsedTime() > 100) {
+            case 13: //Yellow1 drop position
+                if (pathTimer.getElapsedTime() > 500) {
                     robot.rearBasket();
-
                     follower.followPath(yellow1drop);
-
-                    if (atPathEnd() && armSetUp()) {
-                        robot.openClaw();
-                        setPathState(14);
-                    }
+                    setPathState(14);
                 }
 
                 break;
 
-            case 14: //Yellow2pick
-                if (pathTimer.getElapsedTime() > 300) {
+            case 14: //Yellow1 drop sample
+                if (armSetUp() && pathTimer.getElapsedTime() > 500) { //atPathEnd() &&
+                    robot.openClaw();
+                    setPathState(15);
+                }
+                break;
+
+            case 15: //Yellow2 path
+                if (pathTimer.getElapsedTime() > 1500)
+                //if (follower.getPose().getX() > (dropposition.getX() - 1) && follower.getPose().getY() > (dropposition.getY() - 1))
+                    {
+                    follower.followPath(yellow2);
                     robot.pickSample();
 
-                    follower.followPath(yellow2);
-
-                    if (armSetDown() && pathTimer.getElapsedTime()>2500) { //atPathEnd() && armSetDown() &&
-                        robot.closeClaw();
-
-                        setPathState(15);
-                    }
+                    setPathState(16);
                 }
 
                 break;
 
-            case 15: //Yellow2drop
-                if (pathTimer.getElapsedTime() > 100) {
-                    robot.rearBasket();
+            case 16: //Yellow2 pick
+                if (pathTimer.getElapsedTime() > 2000) { //atPathEnd() && armSetDown() &&
+                    robot.closeClaw();
 
-                    follower.followPath(yellow2drop);
-
-                    if (atPathEnd() && armSetUp()) { //isAtEndOfPathAndNotMoving()
-
-                        robot.openClaw();
-
-                        setPathState(16);
-                    }
+                    setPathState(17);
                 }
+                break;
 
+            case 17: //Yellow2 drop position
+                if (pathTimer.getElapsedTime() > 1000) {
+                    robot.rearBasket();
+                    follower.followPath(yellow2drop);
+                    setPathState(18);
+                }
+                break;
+            case 18: // Yellow2 drop sample
+                    if (atPathEnd() && armSetUp()) { //isAtEndOfPathAndNotMoving()
+                        robot.openClaw();
+                        setPathState(19);
+                    }
                     break;
 
-            case 16: //Yellow3pick
+            case 19: //Yellow3 path
                 if (pathTimer.getElapsedTime() > 300) {
-                    robot.moveWristHighBw();
-                    robot.armPos(robot.floorArm, robot.armEPower);
-                    robot.extArmPos(robot.extArmFLoorPick, robot.armEPower);
-                    robot.openClaw();
+                    robot.pickSample();
+                    //robot.moveWristHighBw();
+                    //robot.armPos(robot.floorArm, robot.armEPower);
+                    //robot.extArmPos(robot.extArmFLoorPick, robot.armEPower);
 
                     follower.followPath(yellow3);
+                    robot.openClaw();
+
+                    setPathState(20);
+                }
+                break;
+            case 20: //Yellow3 pick
 
                     if (follower.atParametricEnd() && pathTimer.getElapsedTime()>3000) { //atPathEnd() &&
                         robot.closeClaw();
 
-                        setPathState(17);
+                        setPathState(21);
                     }
-                }
 
                     break;
 
-            case 17: //Yellow3drop
-                if (pathTimer.getElapsedTime() > 100) {
+            case 21: //Yellow3 drop position
+                if (pathTimer.getElapsedTime() > 1000) {
                     robot.rearBasket();
-
                     follower.followPath(yellow3drop);
+                    setPathState(22);
+                }
+                    break;
 
+            case 22: // yellow 3 drop sample
                     if (atPathEnd() && armSetUp() && pathTimer.getElapsedTime()>2000) { //isAtEndOfPathAndNotMoving()
-
                         robot.openClaw();
 
-                        setPathState(18);
+                        setPathState(23);
                     }
-                }
 
                     break;
 
-            case 18: //parking
-                if (pathTimer.getElapsedTime() > 300) {
+            case 23: //parking path
 
+                if (pathTimer.getElapsedTime() > 300) {
                     robot.closeClaw();
                     robot.moveWristCarry();
                     robot.armPos(robot.armAscent, robot.armEPower);
                     robot.extArmPos(0, robot.extArmEPower);
 
                     follower.followPath(ascent);
+                    setPathState(24);
+                }
+                break;
+
+            case 24: //parking end position
 
                     if (atPathEnd() && pathTimer.getElapsedTime()>2500) {
 
                         setPathState(100);
                     }
-                }
+
 
                     break;
 
@@ -348,7 +393,7 @@ public class vvBasketPedro extends OpMode {
 
                 resetRuntime();
 
-                setPathState(9);
+                setPathState(6);
 
             }
     @Override
